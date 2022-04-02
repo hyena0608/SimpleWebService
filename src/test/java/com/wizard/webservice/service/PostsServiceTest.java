d65aa2a673be8e82371ef14179e0d479e2d22fbd
hyena0608 <goatsfish@gmail.com>
2022-04-02 09:43:05 +0900

    test : test추가

---
+++ src/test/java/com/wizard/webservice/service/PostsServiceTest.java
@@ -0,0 +1,55 @@
+package com.wizard.webservice.service;
+
+import com.wizard.webservice.domain.posts.Posts;
+import com.wizard.webservice.domain.posts.PostsRepository;
+import com.wizard.webservice.dto.PostsSaveRequestDto;
+import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.Assert.*;
+
+/**
+ * @author hyena
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsServiceTest {
+
+    @Autowired
+    private PostsService postsService;
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void DTO데이터가_posts테이블에_저장된다() throws Exception {
+        //given
+        PostsSaveRequestDto dto = PostsSaveRequestDto
+                .builder()
+                .author("제이피에이조아@gmail.com")
+                .content("테스트")
+                .title("테스트 타이틀")
+                .build();
+
+        //when
+        postsService.save(dto);
+
+        //then
+        Posts posts = postsRepository.findAll().get(0);
+        assertThat(posts.getAuthor()).isEqualTo(dto.getAuthor());
+        assertThat(posts.getContent()).isEqualTo(dto.getContent());
+        assertThat(posts.getTitle()).isEqualTo(dto.getTitle());
+    }
+
+}
