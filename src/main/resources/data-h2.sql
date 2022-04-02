e6671e92ed00eb618263973d6b99091835ae9a6a
hyena0608 <goatsfish@gmail.com>
2022-04-02 20:04:14 +0900

    test : test 수정

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,4 +1,4 @@
 insert into posts (id, title, author, content, created_date, modified_date)
-values (1, ''테스트1'', ''test1@gmail.com'', ''테스트1의 본문'', now(), now());
+values (1, '테스트1', 'test1@gmail.com', '테스트1의 본문', now(), now());
 insert into posts (id, title, author, content, created_date, modified_date)
-values (2, ''테스트2'', ''test2@gmail.com'', ''테스트2의 본문'', now(), now());
+values (2, '테스트2', 'test2@gmail.com', '테스트2의 본문', now(), now());
