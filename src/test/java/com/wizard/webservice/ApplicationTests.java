c6da2385692f4d3ce6f1e91a6d88ba7c5d3713b7
hyena0608 <goatsfish@gmail.com>
2022-04-02 16:45:50 +0900

    test : test

--- src/test/java/com/wizard/webservice/ApplicationTests.java
+++ src/test/java/com/wizard/webservice/ApplicationTests.java
@@ -1,10 +1,13 @@
 package com.wizard.webservice;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringWebserviceApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	void contextLoads() {
